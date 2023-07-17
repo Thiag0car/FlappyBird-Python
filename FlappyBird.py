6832d5c49aee63a97b16f521d6e1d7f6a607d34a
Thiago <thiago-20021023@hotmail.com>
2023-07-17 19:18:34 -0300

    Add Aula 7/8

--- FlappyBird.py
+++ FlappyBird.py
@@ -108,6 +108,56 @@ class Cano:
         self.altura = random.randrange(50, 450)
         self.pos_topo = self.altura - self.CANO_TOPO.get_height()
         self.pos_base = self.altura + self.DISTANCIA
+    
+    def mover(self):
+       self.x -= self.VELOCIDADE
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO, (self.x , self.pos_topo))
+        tela.blit(self.CANO_BASE, (self.x , self.pos_base))
+
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask=  pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask=  pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - passaro.x , self.pos_topo - round(passaro.y))
+        distancia_base = (self.x - passaro.x , self.pos_base - round(passaro.y))
+
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if base_ponto or topo_ponto:
+            return True
+        else:
+            return False
+
 
 class Chao:
-    pass
+    VELOCIDADE = 5 
+    LARGURA = IMAGEM_CHAO.get_width()
+    IMAGEM = IMAGEM_CHAO
+
+
+    def __init__(self,y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.LARGURA
+
+    def mover(self):
+        self.x1 -= self.VELOCIDADE
+        self.x2 -= self.VELOCIDADE
+
+        if self.x1 + self.LARGURA < 0:
+            self.x1 = self.x1 + self.LARGURA
+        if self.x2 + self.LARGURA < 0:
+            self.x2 = self.x2 + self.LARGURA
+     
+    def desenhar(self, tela):
+        tela.blit(self.IMAGEM, (self.x1 , self.y))
+        tela.blit(self.IMAGEM, (self.x2 , self.y))
+
+    
+
+
+    
